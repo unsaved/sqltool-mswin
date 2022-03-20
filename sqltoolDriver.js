11274f7c64b5449a52f10ccaf333926f934bcaa4
Blaine Simpson <blaine.simpson@admc.com>
2022-03-19 21:50:25 -0400

    Build system fixes and support for powerful JRE env adjustments, most importantly to add JDBC drivers

--- sqltoolDriver.js
+++ sqltoolDriver.js
@@ -2,14 +2,37 @@
 
 "use strict";
 
+const { appVersion } = require("@admc.com/apputil");
+
 const yargs = require("yargs")(process.argv.slice(2)).
   strictOptions().
-  usage("SYNTAX: $0 [-dhqv] -- [sqltool params...]").
+  usage(`SYNTAX:
+$0 [-dhqv] [-m mod/spec] [-p modpath] [-cp classpath] [sqltool params...]
+OR
+$0 [-dhqv] [-m mod/spec] [-p modpath] [-cp classpath] -- [sqltool params...]
+The -- separator is needed (2nd form) if any sqltool param -switches are given.`.
+    replaceAll(/ /g, "\u2009")).
   option("v", { describe: "Verbose", type: "boolean", }).
   option("d", { describe: "Debug logging", type: "boolean", }).
   option("q", {
       describe: "Quiet logging by logging only at level WARN and ERROR",
       type: "boolean",
+  }).
+  option("c", {
+      alias: "cp",
+      describe: "additional ClassPath to be added to JRE environment",
+      type: "string",
+      requiresArg: true,
+  }).
+  option("m", {
+      describe: "override Module.  Replaces 'org.hsqldb.sqltool' entrypoint",
+      type: "string",
+      requiresArg: true,
+  }).
+  option("p", {
+      describe: "additional module Path to be added to JRE environment",
+      type: "string",
+      requiresArg: true,
   }).
   alias("help", "h").
   version(appVersion);
@@ -19,4 +42,12 @@ const progName = yargsDict.$0.replace(/^.*[\\/]/, "");
 if (!yargsDict.d) console.debug = () => {};
 if (yargsDict.q) console.debug = console.log = console.info = () => {};
 
-require("./jreRunner")(["-m", "org.hsqldb.sqltool", ...yargsDict._], yargsDict.v);
+const argsArray = yargsDict._;
+argsArray.unshift(yargsDict.m ? yargsDict.m : "org.hsqldb.sqltool");
+argsArray.unshift("-m");
+if (yargsDict.p) { argsArray.unshift(yargsDict.p); argsArray.unshift("-p"); }
+if (yargsDict.cp) { argsArray.unshift(yargsDict.cp); argsArray.unshift("-cp"); }
+
+console.log(argsArray);
+//require("./jreRunner")(["-m", "org.hsqldb.sqltool", ...yargsDict._], yargsDict.v);
+require("./jreRunner")(argsArray, yargsDict.v);
